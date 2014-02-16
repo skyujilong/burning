032a08511a4a7dd4fefc7fa5c4de72b5165c0358
yujilong <skyujilong@163.com>
2014-02-16 09:50:16 +0800

    trim

--- routes/sdk/sdkPostRoute.js
+++ routes/sdk/sdkPostRoute.js
@@ -10,10 +10,10 @@ var logger = require('./../../common/log').getLogger();
 var Post = require('./../../modules/Post');
 var PostContent = require('./../../modules/PostContent');
 var postService = require('./../../service/PostService').service;
-module.exports = function(app){
+module.exports = function (app) {
 
 
-    app.get('/burning/sdk/getPostList/:appId/:categoryId/:boardId/:pageNum',function(req,res){
+    app.get('/burning/sdk/getPostList/:appId/:categoryId/:boardId/:pageNum', function (req, res) {
 
         var appId = req.param('appId');
         var categoryId = req.param('categoryId');
@@ -21,15 +21,15 @@ module.exports = function(app){
         var pageNum = req.param('pageNum');
         var pageSize = 10;
         if (!util.valiNum(pageNum)) {
-            res.json(200, {rs: 0,msg:'error param'});
+            res.json(200, {rs: 0, msg: 'error param'});
             return;
         }
         postService.getPostList(appId, categoryId, boardId, pageNum, pageSize, function (err, docs) {
             if (err) {
                 logger.error(err);
-                res.json(200, {rs:0,msg:'system error'});
+                res.json(200, {rs: 0, msg: 'system error'});
             } else {
-                res.json(200,{rs:1,postlist:docs});
+                res.json(200, {rs: 1, postlist: docs});
             }
         });
 
